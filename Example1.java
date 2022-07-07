c472b8ec7b803c6e001c7c150d117d0b546ef98c
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-07 16:15:26 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,41 @@
+public class Example1 {
+    public static void main(String[] args) {
+            
+            String name = "Paul";
+            String brand = "idol";
+			double processorspeedInGHz = 2.66;
+            float ramSizeInGB = 8;
+            int HardDiskInGB = 500;
+			byte InchMonitor = 15;
+			double rupeesIn$ = 1760.55;
+			char servicePlan = 'C';
+			boolean OnSiteServicing = true;
+			boolean ExtendedWarranty = false;
+			System.out.println("example name:-" +name);
+			System.out.println("example brand:-" +brand);
+			System.out.println("example processorspeedInGHz:-" +processorspeedInGHz);
+			System.out.println("example ramSizeInGB:-" +ramSizeInGB);
+			System.out.println("example HardDiskInGB:-" +HardDiskInGB);
+			System.out.println("example InchMonitor:-" +InchMonitor);
+			System.out.println("example rupeesIn$:-" +rupeesIn$);
+			System.out.println("example servicePlan:-" +servicePlan);
+			System.out.println("example OnSiteServicing:-" +OnSiteServicing);
+			System.out.println("example ExtendedWarranty:-" +ExtendedWarranty);
+	}
+}
+
+			
+
+			
+
+
+
+				
+			
+				
+
+				
+
+				
+
+      
