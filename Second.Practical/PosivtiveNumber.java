ab0135c7f0af99c70a2fb21ac6d0edb424a65046
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-14 14:11:45 +0530

    Add files via upload

---
+++ Second.Practical/PosivtiveNumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class PosivtiveNumber
+{
+public static void main(String arg[])
+	{ 
+			Scanner s=new Scanner(System.in);
+			System.out.println("enter the number");
+			int num=s.nextInt();
+			if(num<0)
+				{
+					System.out.println("number is negetive");
+				}
+			else
+				{
+					System.out.println("number is positive");
+				}
+	}
+}
