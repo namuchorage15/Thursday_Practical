32cd9408dc9babb841589da222e5460eca6fca14
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-14 14:12:38 +0530

    Add files via upload

---
+++ Second.Practical/DeepTech.java
@@ -0,0 +1,28 @@
+class DeepTech
+{
+public static void main(String arg[])
+    {
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			  if(i%3==0 && i%5==0)
+				    {
+				      System.out.println("multiple by 3 & 5 DeepTech");
+				    }
+			  else if(i%3==0)
+				    {
+				      System.out.println("multiple by 3 Tech");
+				    }
+			  else if(i%5==0)
+				    {
+				      System.out.println("multiple by  5 Tech");
+				    }
+			  else
+				   {
+					System.out.println(i);
+				    }
+
+		}
+
+	}
+}
