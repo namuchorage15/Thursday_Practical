e374d8792bfa7bfd750efb47e5743242812692f7
Namu <108450435+namuchorage15@users.noreply.github.com>
2022-07-29 11:36:23 +0530

    Add files via upload

---
+++ 28-07-2022/SimpleStringTokenizer.java
@@ -0,0 +1,16 @@
+import java.utill.StringTokenizer;
+class Tokenizer
+{
+    public static void main(String args[])
+    {
+        StringTokenizer st = new StringTokenizer("I am a woman"," ");
+        StringTokenizer st = new StringTokenizer("Tomorrow is Monday"," ")
+        while(st.hasMoreTokens())
+        { 
+              System.out.println(st.nextToken());
+         }
+         while(st.hasMoreTokens())
+        { 
+              System.out.println(st.nextToken());
+         }
+}
